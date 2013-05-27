9a33e66f014e051028fe6b15df58b223775a148b
Adam Alpern <adam.alpern@gmail.com>
2013-05-27 10:14:13 -0700

    Indentation

--- include/cinderpane/gizmo/parts/RadialGridPart.h
+++ include/cinderpane/gizmo/parts/RadialGridPart.h
@@ -3,92 +3,92 @@
 #include "cinderpane/gizmo/parts/RadialLinesPart.h"
 
 namespace cinderpane {
-	namespace gizmo {
+  namespace gizmo {
 
-		class RadialGridPart
-			: public PartBase
-		{
-		  public:
-			RadialGridPart()
-				: PartBase(48.0)
-				, m_circleCount(4)
-				, m_lines(48.0, 8.0, 12)
-			{
-			}
+    class RadialGridPart
+        : public PartBase
+    {
+      public:
+        RadialGridPart()
+            : PartBase(48.0)
+            , m_circleCount(4)
+            , m_lines(48.0, 8.0, 12)
+        {
+        }
 
-			RadialGridPart(double radius, int circleCount, int lineCount, bool fill = false)
-				: PartBase(radius)
-				, m_circleCount(circleCount)
-				, m_lines(radius, (int)radius, lineCount)
-			{
-				m_fill = fill;
-				if ( m_circleCount == 0 )
-					m_circleCount = 1;
-				// circleCount includes the outermost
-				m_circleSpacing = radius / circleCount;
-			}
+        RadialGridPart(double radius, int circleCount, int lineCount, bool fill = false)
+            : PartBase(radius)
+            , m_circleCount(circleCount)
+            , m_lines(radius, (int)radius, lineCount)
+        {
+            m_fill = fill;
+            if ( m_circleCount == 0 )
+                m_circleCount = 1;
+            // circleCount includes the outermost
+            m_circleSpacing = radius / circleCount;
+        }
 
-			virtual ~RadialGridPart()
-			{
-			}
+        virtual ~RadialGridPart()
+        {
+        }
 
-			virtual void renderGizmo(GizmoContext &context)
-			{
-				Vec2f position = context.Position + m_position;
-				float radius = m_size.x / 2;
-				context.Cairo.circle(position, radius);
+        virtual void renderGizmo(GizmoContext &context)
+        {
+            Vec2f position = context.Position + m_position;
+            float radius = m_size.x / 2;
+            context.Cairo.circle(position, radius);
 
-				for ( int i = 1; i < m_circleCount; i++ )
-				{
-					context.Cairo.circle(position, radius - ( m_circleSpacing * i ));
-				}
+            for ( int i = 1; i < m_circleCount; i++ )
+            {
+                context.Cairo.circle(position, radius - ( m_circleSpacing * i ));
+            }
 
-				context.draw(true, m_fill, m_state);
+            context.draw(true, m_fill, m_state);
 
-				m_lines.renderGizmo(context);
-			}
+            m_lines.renderGizmo(context);
+        }
 
-			virtual void setDisplayState(DisplayState state)
-			{
-				PartBase::setDisplayState(state);
-				m_lines.setDisplayState(state);
-			}
+        virtual void setDisplayState(DisplayState state)
+        {
+            PartBase::setDisplayState(state);
+            m_lines.setDisplayState(state);
+        }
 
-			virtual void loadXML(const cinder::XmlTree &element,
-								 const PartFactoryRef &factory)
-			{
-				PartBase::loadXML(element, factory);
-				m_circleSpacing = xmlGetChildValue<float>(element, "CircleSpacing", m_circleSpacing);
-				m_circleCount = xmlGetChildValue<int>(element, "CircleCount", m_circleCount);
-				m_lines.setLineCount( xmlGetChildValue<int>(element, "LineCount", m_lines.getLineCount()) );
-				m_lines.setRotation(m_rotation);
-				m_lines.setRotationSpeed(m_rotationSpeedRPM);
-			}
+        virtual void loadXML(const cinder::XmlTree &element,
+                             const PartFactoryRef &factory)
+        {
+            PartBase::loadXML(element, factory);
+            m_circleSpacing = xmlGetChildValue<float>(element, "CircleSpacing", m_circleSpacing);
+            m_circleCount = xmlGetChildValue<int>(element, "CircleCount", m_circleCount);
+            m_lines.setLineCount( xmlGetChildValue<int>(element, "LineCount", m_lines.getLineCount()) );
+            m_lines.setRotation(m_rotation);
+            m_lines.setRotationSpeed(m_rotationSpeedRPM);
+        }
 
-			virtual void storeXML(std::ostream &os) const
-			{
-				xmlBeginPart(os, "RadialGrid");
-				xmlWriteTag(os, "CircleSpacing", m_circleSpacing);
-				xmlWriteTag(os, "CircleCount", m_circleCount);
-				xmlWriteTag(os, "LineCount", m_lines.getLineCount());
-				xmlEndTag(os, "RadialGrid");
-			}
+        virtual void storeXML(std::ostream &os) const
+        {
+            xmlBeginPart(os, "RadialGrid");
+            xmlWriteTag(os, "CircleSpacing", m_circleSpacing);
+            xmlWriteTag(os, "CircleCount", m_circleCount);
+            xmlWriteTag(os, "LineCount", m_lines.getLineCount());
+            xmlEndTag(os, "RadialGrid");
+        }
 
-		  protected:
-			virtual void onRotationChanged(double rotation, double oldRotation)
-			{
-				m_lines.setRotation(rotation);
-			}
+      protected:
+        virtual void onRotationChanged(double rotation, double oldRotation)
+        {
+            m_lines.setRotation(rotation);
+        }
 
-			virtual void onPositionChanged(const Vec2f &pos, const Vec2f &old)
-			{
-				m_lines.setPosition(pos);
-			}
+        virtual void onPositionChanged(const Vec2f &pos, const Vec2f &old)
+        {
+            m_lines.setPosition(pos);
+        }
 
-			double          m_circleSpacing;
-			int             m_circleCount;
-			RadialLinesPart m_lines;
-		};
+        double          m_circleSpacing;
+        int             m_circleCount;
+        RadialLinesPart m_lines;
+    };
 
-	}
+  }
 } // end namespace cinderpane::gizmo
--- include/cinderpane/gizmo/parts/RadialLinesPart.h
+++ include/cinderpane/gizmo/parts/RadialLinesPart.h
@@ -6,110 +6,110 @@
 /// @{
 
 namespace cinderpane {
-	namespace gizmo {
+  namespace gizmo {
 
-		using namespace cinderpane::ext;
+    using namespace cinderpane::ext;
 
-		class RadialLinesPart
-			: public PartBase
-		{
-		  public:
-			RadialLinesPart()
-				: PartBase(48.0)
-				, m_beginDegrees(0.0)
-				, m_endDegrees(360.0)
-				, m_lineLength(24.0)
-				, m_lineCount(12)
-			{
-			}
+    class RadialLinesPart
+        : public PartBase
+    {
+      public:
+        RadialLinesPart()
+            : PartBase(48.0)
+            , m_beginDegrees(0.0)
+            , m_endDegrees(360.0)
+            , m_lineLength(24.0)
+            , m_lineCount(12)
+        {
+        }
 
-			RadialLinesPart(float outerRadius, float lineLength, int lineCount,
-							float beginDegrees = 0.0, float endDegrees = 360.0)
-				: PartBase(outerRadius)
-				, m_beginDegrees(beginDegrees)
-				, m_endDegrees(endDegrees)
-				, m_lineLength(lineLength)
-				, m_lineCount(lineCount)
-			{
-				Calculate();
-			}
+        RadialLinesPart(float outerRadius, float lineLength, int lineCount,
+                        float beginDegrees = 0.0, float endDegrees = 360.0)
+            : PartBase(outerRadius)
+            , m_beginDegrees(beginDegrees)
+            , m_endDegrees(endDegrees)
+            , m_lineLength(lineLength)
+            , m_lineCount(lineCount)
+        {
+            Calculate();
+        }
 
-			RadialLinesPart(const Vec2f &position, float outerRadius, float lineLength, int lineCount,
-							float beginDegrees = 0.0, float endDegrees = 360.0)
-				: PartBase(outerRadius, position)
-				, m_beginDegrees(beginDegrees)
-				, m_endDegrees(endDegrees)
-				, m_lineLength(lineLength)
-				, m_lineCount(lineCount)
-			{
-				Calculate();
-			}
+        RadialLinesPart(const Vec2f &position, float outerRadius, float lineLength, int lineCount,
+                        float beginDegrees = 0.0, float endDegrees = 360.0)
+            : PartBase(outerRadius, position)
+            , m_beginDegrees(beginDegrees)
+            , m_endDegrees(endDegrees)
+            , m_lineLength(lineLength)
+            , m_lineCount(lineCount)
+        {
+            Calculate();
+        }
 
-			virtual ~RadialLinesPart()
-			{
-			}
+        virtual ~RadialLinesPart()
+        {
+        }
 
-			int getLineCount() const { return m_lineCount; }
+        int getLineCount() const { return m_lineCount; }
 
-			void setLineCount(int value)
-			{
-				m_lineCount = value;
-				Calculate();
-			}
+        void setLineCount(int value)
+        {
+            m_lineCount = value;
+            Calculate();
+        }
 
-			virtual void renderGizmo(GizmoContext &context)
-			{
-				double outer_radius = m_size.x / 2;
-				double inner_radius = outer_radius - m_lineLength;
-				double begin  = rotate_value(m_beginDegrees);
-				Vec2f position = context.Position + m_position;
+        virtual void renderGizmo(GizmoContext &context)
+        {
+            double outer_radius = m_size.x / 2;
+            double inner_radius = outer_radius - m_lineLength;
+            double begin  = rotate_value(m_beginDegrees);
+            Vec2f position = context.Position + m_position;
 
-				for ( int i = 0; i <= m_lineCount; i++ )
-				{
-					double angle = cinder::toRadians(begin + ( i * m_incrementDegrees ));
-					Vec2f outer = perimeter(position, outer_radius, angle);
-					Vec2f inner = perimeter(position, inner_radius, angle);
-					context.Cairo.moveTo(outer);
-					context.Cairo.lineTo(inner);
-				}
-				context.draw(true, false, m_state);
-			}
+            for ( int i = 0; i <= m_lineCount; i++ )
+            {
+                double angle = cinder::toRadians(begin + ( i * m_incrementDegrees ));
+                Vec2f outer = perimeter(position, outer_radius, angle);
+                Vec2f inner = perimeter(position, inner_radius, angle);
+                context.Cairo.moveTo(outer);
+                context.Cairo.lineTo(inner);
+            }
+            context.draw(true, false, m_state);
+        }
 
-			virtual void loadXML(const cinder::XmlTree &element,
-								 const PartFactoryRef &factory)
-			{
-				PartBase::loadXML(element, factory);
-				m_beginDegrees = xmlGetChildValue<float>(element, "BeginDegrees",  0.0);
-				m_endDegrees   = xmlGetChildValue<float>(element, "EndDegrees",  180.0);
-				m_lineCount    = xmlGetChildValue<int>(  element, "LineCount",    12);
-				m_lineLength   = xmlGetChildValue<float>(element, "LineLength",    8.0);
-				Calculate();
-			}
+        virtual void loadXML(const cinder::XmlTree &element,
+                             const PartFactoryRef &factory)
+        {
+            PartBase::loadXML(element, factory);
+            m_beginDegrees = xmlGetChildValue<float>(element, "BeginDegrees",  0.0);
+            m_endDegrees   = xmlGetChildValue<float>(element, "EndDegrees",  180.0);
+            m_lineCount    = xmlGetChildValue<int>(  element, "LineCount",    12);
+            m_lineLength   = xmlGetChildValue<float>(element, "LineLength",    8.0);
+            Calculate();
+        }
 
-			virtual void storeXML(std::ostream &os) const
-			{
-				xmlBeginPart(os, "RadialLines");
-				xmlWriteTag(os, "BeginDegrees", m_beginDegrees);
-				xmlWriteTag(os, "EndDegrees",   m_endDegrees);
-				xmlWriteTag(os, "LineCount",    m_lineCount);
-				xmlWriteTag(os, "LineLength",   m_lineLength);
-				xmlEndTag(os, "RadialLines");
-			}
+        virtual void storeXML(std::ostream &os) const
+        {
+            xmlBeginPart(os, "RadialLines");
+            xmlWriteTag(os, "BeginDegrees", m_beginDegrees);
+            xmlWriteTag(os, "EndDegrees",   m_endDegrees);
+            xmlWriteTag(os, "LineCount",    m_lineCount);
+            xmlWriteTag(os, "LineLength",   m_lineLength);
+            xmlEndTag(os, "RadialLines");
+        }
 
-		  protected:
-			void Calculate()
-			{
-				m_incrementDegrees = ( m_endDegrees - m_beginDegrees ) / m_lineCount;
-			}
+      protected:
+        void Calculate()
+        {
+            m_incrementDegrees = ( m_endDegrees - m_beginDegrees ) / m_lineCount;
+        }
 
-			float m_beginDegrees;
-			float m_endDegrees;
-			float m_lineLength;
-			float m_incrementDegrees;
-			int   m_lineCount;
-		};
+        float m_beginDegrees;
+        float m_endDegrees;
+        float m_lineLength;
+        float m_incrementDegrees;
+        int   m_lineCount;
+    };
 
-	}
+  }
 } // end namespace cinderpane::gizmo
 
 /// @}
